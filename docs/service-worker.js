17a6e7c5deb27e50024b8fe2629bbe55aa111c31
Hmdgt <hmdgit01@gmail.com>
2026-02-24 14:16:21 +0000

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,3 +1,7 @@
-self.addEventListener("install", () => {
-  console.log("PWA instalada");
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  clients.claim();
 });
