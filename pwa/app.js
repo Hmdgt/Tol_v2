e98d4d9d2b48e196462c0e6c0f1779db2884394c
Hmdgt <hmdgit01@gmail.com>
2026-02-26 17:08:08 +0000

    Delete pwa directory

--- pwa/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("uploadBtn").addEventListener("click", () => {
-  const file = document.getElementById("cameraInput").files[0];
-  if (!file) {
-    alert("Escolhe ou tira uma foto primeiro.");
-    return;
-  }
-  uploadToGitHub(file);
-});
